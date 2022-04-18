016c2d05d056b04c630935bb2ba434cc266005aa
PeterTodorovv <petertodorovv@gmail.com>
2022-04-18 20:56:50 +0300

    04/18

--- 02.CRUD/Exercise.sql
+++ 02.CRUD/Exercise.sql
@@ -31,3 +31,11 @@ SELECT FirstName +' ' + MiddleName + ' '  + LastName AS 'Full Name'
 FROM Employees
 WHERE Salary IN (25000, 14000, 12500, 23600)
 GO
+
+SELECT FirstName, LastName FROM Employees
+WHERE ManagerID IS NULL
+GO
+
+SELECT FirstName, LastName, Salary FROM Employees
+WHERE Salary > 50000
+ORDER BY Salary DESC
