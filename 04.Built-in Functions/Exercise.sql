dbea215a2742d8bcdb011def10123fa5b42bcc18
PeterTodorovv <petertodorovv@gmail.com>
2022-05-06 14:22:05 +0300

    05/06

--- 04.Built-in Functions/Exercise.sql
+++ 04.Built-in Functions/Exercise.sql
@@ -59,3 +59,47 @@ USE Geography
 SELECT CountryName, IsoCode FROM Countries
 WHERE CountryName LIke '%a%a%a%'
 ORDER BY IsoCode
+GO
+
+SELECT PeakName, RiverName,
+LOWER(CONCAT(LEFT(PeakName, LEN(PeakName) - 1), RiverName)) AS MIX
+FROM Peaks, Rivers 
+WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY MIX
+GO
+
+USE Diablo
+SELECT [Name], 
+FORMAT([Start], 'yyyy-MM-dd') AS [Start] 
+FROM Games
+WHERE DATEPART(YEAR ,[Start]) IN (2011, 2012)
+ORDER BY [Start], [Name]
+GO
+
+SELECT Username, 
+RIGHT(Email, LEN(Email) - CHARINDEX('@', Email)) AS [Email Provider]
+FROM Users
+ORDER BY [Email Provider], Username
+GO
+
+SELECT Username, IpAddress FROM Users
+WHERE IpAddress LIKE '___.1%.%.___'
+ORDER BY Username
+GO
+
+SELECT [Name] AS Game,
+CASE
+	WHEN DATEPART(HOUR, [Start]) >= 0 AND DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+	WHEN DATEPART(HOUR, [Start]) >= 12 AND DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
+	ELSE 'Evening'
+	END AS [Part of the day],
+CASE 
+	WHEN Duration <=3 THEN 'Extra Short'
+	WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+	WHEN Duration > 6 THEN 'Long'
+	ELSE 'Extra Long' 
+	END AS Duration
+FROM Games
+ORDER BY Game, Duration
+GO
+
