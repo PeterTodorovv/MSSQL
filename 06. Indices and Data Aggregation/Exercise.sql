fb34965ed2824af080da715bbf244d24aa0cc829
PeterTodorovv <petertodorovv@gmail.com>
2022-05-13 20:55:57 +0300

    05/13

--- 06. Indices and Data Aggregation/Exercise.sql
+++ 06. Indices and Data Aggregation/Exercise.sql
@@ -32,4 +32,30 @@ GO
 SELECT DepositGroup, SUM(DepositAmount) AS 'TotalSum'
 FROM WizzardDeposits
 WHERE MagicWandCreator = 'Ollivander family'
-GROUP BY DepositGroup, DepositAmount
+GROUP BY DepositGroup
+HAVING SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC
+GO
+
+SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge) AS MinDepositCharge
+FROM WizzardDeposits
+GROUP BY DepositGroup, MagicWandCreator
+ORDER BY MagicWandCreator, DepositGroup
+GO
+
+SELECT AgeGroup, COUNT(AgeGroup) AS WizardCount
+FROM(
+SELECT 
+CASE
+	WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	ELSE '[61+]'
+END AS AgeGroup
+FROM WizzardDeposits
+) AS AgeGroups
+GROUP BY AgeGroup
+GO
